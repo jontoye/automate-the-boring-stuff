78fc4e9daaa33525b23cc4707d46969f6c9b9d55
Jonathan Toye <jontoye@gmail.com>
2021-03-13 22:54:25 -0500

    Changed random.randint to random.choice

--- 11-Debugging/debug_coin_toss.py
+++ 11-Debugging/debug_coin_toss.py
@@ -4,7 +4,7 @@ guess = ""
 while guess not in ("heads", "tails"):
     print("Guess the coin toss! Enter heads or tails: ")
     guess = input()
-toss = random.randint(0, 1)  # 0 is tails, 1 is heads
+toss = random.choice(("heads", "tails"))
 if toss == guess:
     print("You got it")
 else:
